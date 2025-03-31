28ed48a384a8bfdc76bd8f7b4f9ea0c10a8d3ffd
trippleflipp <trippleflippbeats@gmail.com>
2025-03-31 21:14:00 +0700

    Added leader table sample + changed theme and project name

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     AuthGuard,
-    RoleGuard, provideAnimationsAsync()
+    RoleGuard, provideAnimationsAsync(), provideAnimationsAsync()
   ]
 };
